af31d8240e2db92f822c4dec1cec6bb9d990b7a1
rigojr <rigojunnior@gmail.com>
2023-12-31 13:06:22 -0500

    feat: improve crossword cell implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,94 @@
+'use client'
+
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
 import Cell from '@/components/cell';
 
+/**
+ * TODO: find better name.
+ */
+class BoxLetter {
+  private payload: string | undefined;
+  private visiblePayload: string | undefined;
+  private uuid: string;
+
+  constructor(payload: string) {
+    this.payload = payload === '_' ? undefined : payload;
+    this.visiblePayload = payload === '_' ? undefined : '';
+    this.uuid = uuid();
+  }
+
+  public setVisiblePayload(value: string) {
+    this.visiblePayload = value;
+  }
+
+  public getPayload(): string | undefined {
+    return this.payload;
+  }
+
+  public getVisiblePayload(): string | undefined {
+    return this.visiblePayload;
+  }
+
+  public getUuid(): string {
+    return this.uuid;
+  }
+}
+
 export default function Home() {
+  const testDAta =
+    [
+      ['H', 'O', 'O', 'K', 'S', '_', '_', '_', '_', '_'],  // Row 1
+      ['_', '_', '_', '_', 'T', '_', '_', '_', '_', '_'],  // Row 2
+      ['_', '_', '_', '_', 'A', '_', '_', 'J', '_', '_'],  // Row 3
+      ['_', '_', '_', '_', 'T', '_', '_', 'S', '_', '_'],  // Row 4
+      ['_', '_', '_', 'R', 'E', 'D', 'U', 'X', '_', '_'],  // Row 5
+      ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],  // Row 6
+      ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],  // Row 7
+      ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],  // Row 8
+      ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],  // Row 9
+      ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']   // Row 10
+    ];
+
+  const [matrix, setMatrix] = useState(testDAta.map((row) => {
+    return row.map((value) => {
+      return new BoxLetter(value);
+    });
+  }))
+
+  /**
+   * TODO:
+   */
+  function updateMatrix(value: string, rowIndex: number, colIndex: number): void {
+    const newMatrix = Array.from(matrix);
+
+    newMatrix[rowIndex][colIndex].setVisiblePayload(value);
+
+    setMatrix(newMatrix);
+  }
+
   return (
     <main className="min-h-screen p-24">
-      <div className="flex">
-        <Cell payload='a' count={3}/>
-        <Cell payload='a' />
-      </div>
-      <div className="flex">
-        <Cell payload='a' isEmpty/>
-        <Cell payload='a' />
-      </div>
+      {
+        matrix.map((row, rowIndex) => {
+          return <div className='flex' key={uuid()}>
+            {
+              row.map((value, colIndex) => {
+                return (
+                  <Cell
+                    key={value.getUuid()}
+                    payload={value.getVisiblePayload() ?? ''}
+                    isEmpty={value.getVisiblePayload() === undefined}
+                    isVisible={value.getPayload() !== undefined}
+                    onChange={(value) => updateMatrix(value, rowIndex, colIndex)}
+                  />
+                )
+              })
+            }
+          </div>
+        })
+      }
     </main>
   )
 }
--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent } from "react";
+
 export interface Props {
   /**
    * The cell payload.
@@ -10,29 +12,47 @@ export interface Props {
   isEmpty?: boolean;
 
   /**
+   * TODO:
+   */
+  isVisible?: boolean;
+
+  /**
    * The cell upper left-hand count.
    */
   count?: number;
+
+  /**
+   * Occurs when the input text has been changed.
+   */
+  onChange: (value: string) => void
 }
 
 export default function Cell({
   payload,
   isEmpty = false,
-  count
+  isVisible = true,
+  count,
+  onChange
 }: Props) {
 
   function getBorderedStyle(): string {
-    if (isEmpty) {
+    if (!isVisible) {
       return '';
     }
 
     return 'border border-purple-400 rounded';
   }
 
+  function doOnChange(e: ChangeEvent<HTMLInputElement>, payload: string): void {
+    const value = e.target.value.replace(payload, '');
+
+    onChange(value.toUpperCase());
+  }
+
   return (
     <div className={`relative flex items-center justify-center m-1 xs:h-6 xs:w-6 sm:h-8 sm:w-8 lg:w-10 lg:h-10 xl:h-12 xl:w-12 ${getBorderedStyle()}`}>
       <span className="uppercase">
-        { isEmpty ? '' : payload }
+        { isEmpty ? '' : <input className="block w-full bg-transparent text-center" type="text" value={payload} onChange={(e) => doOnChange(e, payload)} />}
       </span>
       {
         count ? <span className="absolute top-0 left-0 p-1 text-xs">{count}</span> : ''
